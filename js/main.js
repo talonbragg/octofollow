979e25b7e6e95b392311645aca61e7db6fe85fc9
Talon <pvpgamecrash@gmail.com>
2018-10-29 21:22:24 -0700

    meta data in html and static display of followers

--- js/main.js
+++ js/main.js
@@ -1,17 +1,23 @@
 function openUser() {
   var usernameInput = document.getElementById('username');
+  var usernameFillElement = document.getElementById('usernameFill');
+  var followersElement = document.getElementById('followers')
   function dumpResponse() {
     console.log(request);
     if(request.readyState === 4) {
       var responseObj = JSON.parse(request.responseText);
+      var followers = responseObj.followers;
       console.log(this.responseText);
       console.log(responseObj.name + " has " + responseObj.public_repos + " public repositories!");
+      //Changing the innerHTML of id="followers"
+      followersElement.innerHTML = followers.toString();
     }
     else {
       console.log(arguments, request);
     }
   }
   var user = usernameInput.value;
+  usernameFillElement.innerHTML = user;
   var request = new XMLHttpRequest();
   request.onreadystatechange = dumpResponse;
   request.open('GET', 'https://api.github.com/users/' + user);
